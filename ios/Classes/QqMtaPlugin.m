b9914ddef51949f38cd4326bbe73289c2eb855b2
赵龙 <zhaolong@zhuimengkeji.com>
2020-07-01 17:30:57 +0800

    完成ios初始化

--- ios/Classes/QqMtaPlugin.m
+++ ios/Classes/QqMtaPlugin.m
@@ -1,20 +1,15 @@
 #import "QqMtaPlugin.h"
-#if __has_include(<qq_mta/qq_mta-Swift.h>)
-#import <qq_mta/qq_mta-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "qq_mta-Swift.h"
-#endif
+#import "MTA.h"
+#import "MTAConfig.h"
 
-static NSString CHANNEL_NAME = @"qq_mta";
+static NSString *CHANNEL_NAME = @"qq_mta";
 @implementation QqMtaPlugin
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:CHANNEL_NAME
             binaryMessenger:[registrar messenger]];
+    QqMtaPlugin* instance = [[QqMtaPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
   //[SwiftQqMtaPlugin registerWithRegistrar:registrar];
 }
@@ -24,7 +19,7 @@ static NSString CHANNEL_NAME = @"qq_mta";
     NSString *iosAppKey = call.arguments[@"iosAppKey"];
     BOOL debugEnabled = call.arguments[@"debugEnabled"];
 
-    [[MTAConfig getInstance] setDebugEnable:YES];
+    [[MTAConfig getInstance] setDebugEnable:debugEnabled];
     [MTA startWithAppkey:iosAppKey];
   }
 }
