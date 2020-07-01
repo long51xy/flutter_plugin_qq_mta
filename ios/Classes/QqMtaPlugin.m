4ae0f3c9059f5873f2cd5272ffe8fca5b20d24e1
zhaolong <zhaolong@zhuimengkeji.com>
2020-07-01 16:58:04 +0800

    增加ios初始化方法

--- ios/Classes/QqMtaPlugin.m
+++ ios/Classes/QqMtaPlugin.m
@@ -8,8 +8,24 @@
 #import "qq_mta-Swift.h"
 #endif
 
+static NSString CHANNEL_NAME = @"qq_mta";
 @implementation QqMtaPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftQqMtaPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:CHANNEL_NAME
+            binaryMessenger:[registrar messenger]];
+  [registrar addMethodCallDelegate:instance channel:channel];
+  //[SwiftQqMtaPlugin registerWithRegistrar:registrar];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"init" isEqualToString:call.method]) {
+    NSString *iosAppKey = call.arguments[@"iosAppKey"];
+    BOOL debugEnabled = call.arguments[@"debugEnabled"];
+
+    [[MTAConfig getInstance] setDebugEnable:YES];
+    [MTA startWithAppkey:iosAppKey];
+  }
 }
 @end
